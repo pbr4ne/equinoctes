180d8870c24e5c5960f7068689988f608c4bf7ca
pbr4ne <pbr4ne@protonmail.com>
2025-01-10 08:03:43 -0800

    feat: power popover

--- src/components/layout/Power.vue
+++ src/components/layout/Power.vue
@@ -1,9 +1,21 @@
 <template>
-    <n-text v-if="faction === 'sun'" class="power solar">Aurum: </n-text>
-    <n-text v-else class="power lunar">Nocturne: </n-text>
-    <n-text :class="['power', faction === 'sun' ? 'solar' : 'lunar']">
-      {{  Intl.NumberFormat().format(Math.round(store.factions[props.faction].power)) }}
-    </n-text>
+  <n-popover 
+    trigger="hover" 
+    placement="top"
+    :theme-overrides="faction === 'sun' ? sunPopoverThemeOverride : moonPopoverThemeOverride"
+  >
+    <template #trigger>
+      <div>
+        <n-text v-if="faction === 'sun'" class="power solar">Aurum: </n-text>
+        <n-text v-else class="power lunar">Nocturne: </n-text>
+        <n-text :class="['power', faction === 'sun' ? 'solar' : 'lunar']">
+          {{  Intl.NumberFormat().format(Math.round(store.factions[props.faction].power)) }}
+        </n-text>
+      </div>
+    </template>
+    <span v-if="faction === 'sun'">The power of the sun.</span>
+    <span v-else>The power of the moon.</span>
+  </n-popover>
 </template>
 
 <script setup lang="ts">
@@ -12,6 +24,20 @@ import { FactionKey } from '../../utilities/types';
 
 const props = defineProps<{ faction: FactionKey }>()
 const store = useStore();
+
+const sunPopoverThemeOverride = {
+  "color": "#9e2a2b",
+  "textColor": "#e9c46a",
+  "titleTextColor": "#e9c46a",
+  "borderColor": "#e9c46a"
+}
+
+const moonPopoverThemeOverride = {
+  "color": "#caf0f8",
+  "textColor": "#264653",
+  "titleTextColor": "#264653",
+  "borderColor": "#264653"
+}
 </script>
 
 <style scoped>
