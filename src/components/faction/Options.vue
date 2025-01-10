f1f16c9d74383767baec7c722adc5ff45fb70484
pbr4ne <pbr4ne@protonmail.com>
2025-01-09 18:32:21 -0800

    feat: credits

--- src/components/faction/Options.vue
+++ src/components/faction/Options.vue
@@ -1,9 +1,30 @@
 <template>
-    <n-button 
-      :class="['buildingButton', `buildingButton-${faction}`]" 
-      :color="faction === 'sun' ? '#9e2a2b' : '#caf0f8'"
-      @click="reset">reset
-    </n-button>
+  <n-space vertical>
+    <n-space>
+      <n-button 
+        :class="['buildingButton', `buildingButton-${faction}`]" 
+        :color="faction === 'sun' ? '#9e2a2b' : '#caf0f8'"
+        @click="reset">Hard Reset
+      </n-button>
+      <n-button 
+        :class="['buildingButton', `buildingButton-${faction}`]" 
+        :color="faction === 'sun' ? '#9e2a2b' : '#caf0f8'"
+        @click="reset">Reset Run
+      </n-button>
+    </n-space>    
+    <n-card 
+      title="Credits"
+      :theme-overrides="faction === 'sun' ? sunCardThemeOverride : moonCardThemeOverride"
+      style="font-family: Texturina, serif; border-radius: 20px;"
+    >
+      <p v-if="faction === 'sun'">Radiant Lady: <a href="https://github.com/pbr4ne" :class="[`link-${faction}`]" target="_blank">pbrane</a></p>
+      <p v-if="faction === 'moon'">Moon Lord: pbrane</p>
+      <p>Writing: <a href="https://jamesfunfer.com" :class="[`link-${faction}`]" target="_blank">James Funfer</a></p>
+      <p>Music: <a href="https://www.youtube.com/user/Kitchen1066" :class="[`link-${faction}`]" target="_blank">terranaut1066</a></p>
+      <p>Did Some Math: Travis</p>
+      <p>Special Thanks: Cheese, Julz, Kakumeii, Sof</p>
+    </n-card>
+  </n-space>
 </template>
 
 <script setup lang="ts">
@@ -14,6 +35,20 @@ defineProps<{ faction: FactionKey }>()
 
 function reset() {
   useStore().reset();
+}
+
+const sunCardThemeOverride = {
+  "color": "#e9c46a",
+  "textColor": "#9e2a2b",
+  "titleTextColor": "#9e2a2b",
+  "borderColor": "#9e2a2b",  
+}
+
+const moonCardThemeOverride = {
+  "color": "#264653",
+  "textColor": "#caf0f8",
+  "titleTextColor": "#caf0f8",
+  "borderColor": "#caf0f8",
 }
 </script>
 
@@ -34,6 +69,14 @@ function reset() {
 .buildingButton-moon {
   border: 1px solid #caf0f8;
   background-color: #264653;
+  color: #caf0f8;
+}
+
+.link-sun {
+  color: #9e2a2b;
+}
+
+.link-moon {
   color: #caf0f8;
 }
 </style>
